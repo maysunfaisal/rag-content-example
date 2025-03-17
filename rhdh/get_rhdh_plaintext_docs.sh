fa0ea4022bef9a5ca550d1e7a89a9390106cfa7d
Maysun J Faisal <maysunaneek@gmail.com>
2025-03-17 16:53:10 -0400

    mod 1
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

---
+++ rhdh/get_rhdh_plaintext_docs.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eou pipefail
+
+RHDH_VERSION=$1
+
+# trap "rm -rf red-hat-developers-documentation-rhdh" EXIT
+
+# rm -rf rhdh-product-docs-plaintext/${RHDH_VERSION}
+
+# git clone --single-branch --branch release-${RHDH_VERSION} https://github.com/redhat-developer/red-hat-developers-documentation-rhdh
+
+pdm run -v rhdh_convert_it_all.py \
+    -r https://github.com/redhat-developer/red-hat-developers-documentation-rhdh.git \
+    -o rhdh-product-docs-plaintext/${RHDH_VERSION} \
+    -t https://raw.githubusercontent.com/maysunfaisal/rag-content/refs/heads/poc-2/scripts/asciidoctor-text/rhdh_topic_map.yaml \
+    -a https://raw.githubusercontent.com/maysunfaisal/rag-content/refs/heads/poc-2/scripts/asciidoctor-text/rhdh_attributes.yaml
