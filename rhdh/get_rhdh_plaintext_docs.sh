1dce0f1cc32a9019f4fd986caa27a0203549306b
Maysun J Faisal <maysunaneek@gmail.com>
2025-03-17 17:18:03 -0400

    rename
    Signed-off-by: Maysun J Faisal <maysunaneek@gmail.com>

--- rhdh/get_rhdh_plaintext_docs.sh
+++ rhdh/get_rhdh_plaintext_docs.sh
@@ -9,7 +9,7 @@ RHDH_VERSION=$1
 
 # git clone --single-branch --branch release-${RHDH_VERSION} https://github.com/redhat-developer/red-hat-developers-documentation-rhdh
 
-pdm run -v rhdh_convert_it_all.py \
+pdm run -v rhdh.py \
     -r https://github.com/redhat-developer/red-hat-developers-documentation-rhdh.git \
     -o rhdh-product-docs-plaintext/${RHDH_VERSION} \
     -t https://raw.githubusercontent.com/maysunfaisal/rag-content/refs/heads/poc-2/scripts/asciidoctor-text/rhdh_topic_map.yaml \
